f3177b58aacf4a98b79c2b7b2223fb34f08c4f2e
Sergiu Danalachi <danalachi.sergiu@gmail.com>
2022-11-27 19:13:15 +0200

    refactor(vae): hparams

--- 05-03-Variational-Autoencoder/VariationalAutoEncoder.py
+++ 05-03-Variational-Autoencoder/VariationalAutoEncoder.py
@@ -1,9 +1,11 @@
 from argparse import ArgumentParser
 
+import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from pytorch_lightning import Trainer
+from pytorch_lightning import loggers as pl_loggers
 from pytorch_lightning.core import LightningModule
 from torch import optim
 from torch.utils.data import DataLoader
@@ -11,13 +13,7 @@ from torchvision import datasets, transforms
 
 
 class VariationalAutoEncoder(LightningModule):
-    def __init__(self,
-                 data_root: str = '../datasets',
-                 batch_size: int = 64,
-                 latent_variables: int = 20,
-                 learning_rate: float = 0.01,
-                 **kwargs
-                 ):
+    def __init__(self, **kwargs):
         super().__init__()
         self.save_hyperparameters()
         self.encoder = nn.Sequential(
@@ -28,16 +24,16 @@ class VariationalAutoEncoder(LightningModule):
             nn.ReLU(True),
             nn.MaxPool2d(2, 2)
         )
-        self.fc_mu = nn.Linear(196, latent_variables)
-        self.fc_logvar = nn.Linear(196, latent_variables)
-        self.dec_fc = nn.Linear(latent_variables, 196)
+        self.fc_mu = nn.Linear(196, self.hparams.latent_variables)
+        self.fc_logvar = nn.Linear(196, self.hparams.latent_variables)
+        self.dec_fc = nn.Linear(self.hparams.latent_variables, 196)
         self.decoder = nn.Sequential(
             nn.ConvTranspose2d(4, 16, kernel_size=2, stride=2),
             nn.ReLU(True),
             nn.ConvTranspose2d(16, 1, kernel_size=2, stride=2),
             nn.Sigmoid()
         )
-        self.example_input_array = torch.rand(batch_size, 1, 28, 28)
+        self.example_input_array = torch.rand(self.hparams.batch_size, 1, 28, 28)
 
     def encode(self, x):
         nr_batches = x.shape[0]
@@ -64,13 +60,8 @@ class VariationalAutoEncoder(LightningModule):
         x = self.decode(z)
         return x, mu, logvar
 
-    def sample(self, num_samples):
-        z = torch.randn(num_samples, self.hparams.latent_variables)
-        samples = self.decode(z)
-        return samples
-
     def loss_function(self, x, x_hat, mu, logvar):
-        loss = F.binary_cross_entropy(x_hat.view(-1, 28*28), x.view(-1, 28*28), reduction='sum')
+        loss = F.binary_cross_entropy(x_hat.view(-1, 28 * 28), x.view(-1, 28 * 28), reduction='sum')
         kl_divergence = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
         return loss + kl_divergence
 
@@ -92,7 +83,7 @@ class VariationalAutoEncoder(LightningModule):
 
     def configure_optimizers(self):
         optimizer = optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
-        return [optimizer]
+        return optimizer
 
     def prepare_data(self):
         datasets.MNIST(self.hparams.data_root, train=True, download=True)
@@ -100,33 +91,40 @@ class VariationalAutoEncoder(LightningModule):
 
     def setup(self, stage):
         transform = transforms.Compose([transforms.ToTensor()])
-        train = datasets.MNIST(self.hparams.data_root, train=True, download=False, transform=transform)
-        self.mnist_train, self.mnist_val = torch.utils.data.random_split(train, [50000, 10000])
-        self.mnist_test = datasets.MNIST(self.hparams.data_root, train=False, download=False, transform=transform)
+        if stage == 'fit':
+            train = datasets.MNIST(self.hparams.data_root, train=True, download=False, transform=transform)
+            self.mnist_train, self.mnist_val = torch.utils.data.random_split(train, [50000, 10000])
+        elif stage == 'test':
+            self.mnist_test = datasets.MNIST(self.hparams.data_root, train=False, download=False, transform=transform)
 
     def train_dataloader(self):
-        return DataLoader(self.mnist_train, batch_size=self.hparams.batch_size, num_workers=4, shuffle=True)
+        return DataLoader(self.mnist_train, batch_size=self.hparams.batch_size, num_workers=self.hparams.num_workers,
+                          shuffle=True)
 
     def val_dataloader(self):
-        return DataLoader(self.mnist_val, batch_size=self.hparams.batch_size, num_workers=4)
+        return DataLoader(self.mnist_val, batch_size=self.hparams.batch_size, num_workers=self.hparams.num_workers)
 
     def test_dataloader(self):
-        return DataLoader(self.mnist_test, batch_size=self.hparams.batch_size, num_workers=4)
+        return DataLoader(self.mnist_test, batch_size=self.hparams.batch_size, num_workers=self.hparams.num_workers)
 
     @staticmethod
     def add_model_specific_args():
         parser = ArgumentParser()
+        parser.add_argument('--data_root', default='../datasets', type=str, help='path to dataset')
         parser.add_argument('--latent_variables', default=5, type=int, help='the number of latent variables to learn')
         parser.add_argument('--learning_rate', default=0.001, type=float, help='the learning rate')
+        parser.add_argument('--batch_size', default=64, type=int, help='the batch size')
+        parser.add_argument('--num_workers', default=4, type=int, help='the number of processes used for data loading')
         return parser
+
 
 def main(args):
     model = VariationalAutoEncoder(**vars(args))
     trainer = Trainer.from_argparse_args(args)
-    trainer.fit(model)
-
-    # Manually call test which runs the test loop and logs accuracy and loss
-    trainer.test()
+    tb_logger = pl_loggers.TensorBoardLogger(save_dir=os.getcwd(), name='lightning_logs/')
+    trainer.logger = tb_logger
+    trainer.fit(model=model)
+    trainer.test(model=model)
 
 
 if __name__ == '__main__':
